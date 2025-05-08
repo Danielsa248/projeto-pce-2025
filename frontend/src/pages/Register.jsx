7154cc1438661106b7160076b2ff692a6541682e
Frederico Faria <fariavnf.fredy@gmail.com>
2025-05-08 19:07:14 +0100

    remove comment

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from 'react';
 
-// Use the same FormRender component with the individuo type
 const FormRender = lazy(() => import('../FormRender.jsx'));
 
 export default function Register() {
